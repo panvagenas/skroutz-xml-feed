217d02042f9713d2c13af8572c327007ec7bcf43
Pan <pan.vagenas@gmail.com>
2017-09-12 09:05:59 +0300

    Save logs at the end of the generation procedure in order to limit DB queries

--- src/XmlGenerator/Logger/Handlers/DBHandler.php
+++ src/XmlGenerator/Logger/Handlers/DBHandler.php
@@ -56,6 +56,9 @@ class DBHandler extends AbstractProcessingHandler {
             'message' => $record['formatted'],
             'time'    => $datetime->format( 'U' ),
         );
+	}
+
+	public function __destruct() {
         update_option( $this->logName, $this->logs );
     }
 
